973f1586a864c124203ed9426407176475d86644
Nguyen Hoan <96786536+5h4s1@users.noreply.github.com>
2022-03-17 00:03:34 +0700

    update

--- buoi1/bai1.c
+++ buoi1/bai1.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
+int dequy( int n){
+    if(n == 0) return 2;
+    return (dequy(n-1) * dequy(n-1));
+}
 int main(){
-    printf("Hello");
+    int n;
+    scanf("%d", &n);
+    printf("%d", dequy(n));
     return 0;
 }
