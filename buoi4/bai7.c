2a43c7347ce841ae4c51ca9f90e8784e06bdb18f
Nguyen Hoan <96786536+5h4s1@users.noreply.github.com>
2022-03-24 17:12:39 +0700

    update

--- buoi4/bai7.c
+++ buoi4/bai7.c
@@ -10,16 +10,39 @@ void rand_array(int a[][100], int d, int c)
         for (j = 0; j < c; j++)
         {
             a[i][j] = 1 + rand() % (99);
-            printf("%d ", a[i][j]);
+            printf("a[%d%d]%d ", i, j, a[i][j]);
         }
         printf("\n");
     }
 }
 
+int max_value(int a[][100], int d, int c, int x, int y)
+{
+
+    if (a[y + 1][x + 1] < a[y + 1][x])
+    {
+        if (x == 0)
+            return x + 1;
+        else if (a[y + 1][x + 1] < a[y + 1][x - 1] && x != 0 && x != c - 1)
+            return x + 1;
+    }
+    if (a[y + 1][x - 1] < a[y + 1][x])
+    {
+        if (x == c - 1)
+            return x - 1;
+        else if (a[y + 1][x - 1] < a[y + 1][x + 1] && x != 0 && x != c - 1)
+            return x - 1;
+    }
+    return x;
+}
+
 int min_material(int a[][100], int d, int c, int x, int y)
 {
-    if (y == -1) return 0;
-    // if ()
+    if (y == d - 1)
+        return 0;
+    x = max_value(a, d, c, x, y);
+    printf("%d %d\n", x, a[y + 1][x]);
+    return a[y + 1][x] + min_material(a, d, c, x, y + 1);
 }
 
 int main()
@@ -29,6 +52,8 @@ int main()
     scanf("%d%d", &d, &c);
     int a[d][c];
     rand_array(a, d, c);
+    scanf("%d%d", &x, &y);
 
+    printf("%d", min_material(a, d, c, x, y) + a[y][x]);
     return 0;
-}   
+}
